1a649e977d1fbb88972a9b289e43daf3650d1ade
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-01-19 13:10:59 +0530

    Testing phase

---
+++ Array/Hello.py
@@ -0,0 +1,2 @@
+print('Hello World')
+print('The number is = ', int(input()))
--- Array/MinMax.cpp
+++ Array/MinMax.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    printf("Hello World!\n");
+    int a;
+    scanf("%d", &a);
+    printf("%d", a);
 }
