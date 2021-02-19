f0a973315824b16d9c592b10e3989898cd2e079a
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-02-19 14:02:29 +0530

    Merge Sort added

---
+++ 450 Questions for DSA/Array/16_Count_Inversions.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pi (3.141592653589)
+#define mod 1000000007
+#define int long long
+#define float long double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+
+bool isPrime(int n) 
+{ 
+    // Corner cases 
+    if (n <= 1)  return false; 
+    if (n <= 3)  return true; 
+  
+    // This is checked so that we can skip  
+    // middle five numbers in below loop 
+    if (n%2 == 0 || n%3 == 0) return false; 
+  
+    for (int i=5; i*i<=n; i=i+6) 
+        if (n%i == 0 || n%(i+2) == 0) 
+           return false; 
+  
+    return true; 
+} 
+
+int32_t main(){
+fast
+
+
+// ||| Author: Abhrajyoti Kundu |||
+// ||| DS-ALGO Lover |||
+
+
+int t=1;
+cin>>t;
+while(t--){
+    
+}
+return 0;
+}
