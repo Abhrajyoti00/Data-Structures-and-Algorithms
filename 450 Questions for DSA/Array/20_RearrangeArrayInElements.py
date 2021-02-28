26576bfbd0160d0677b6228d7d18529d436f8535
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-02-28 22:23:34 +0530

    Rearrange Array in Elements

---
+++ 450 Questions for DSA/Array/20_RearrangeArrayInElements.py
@@ -0,0 +1,33 @@
+def Rearrange(arr):
+    i = 0
+    j = len(arr)-1
+    while(i<j):
+        if arr[i]>=0 and arr[j]<0:
+            i+=1
+            j-=1
+        elif arr[i]>=0 and arr[j]>0:
+            i+=1
+        elif arr[i]<0 and arr[j]<0:
+            j-=1
+        elif arr[i]<0 and arr[j]>=0:
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    #Now i is pointing to the start of neg array, 2 3 4 -1 -5 -6
+    i+=1
+    k = 0
+    start_of_neg = i
+
+    while(i<len(arr)):
+        arr[k], arr[i] = arr[i], arr[k]
+        i+=1
+        k+=2
+        
+    print(arr)
+    return
+
+
+    
+
+arr = [-5, -2, 5, 2, 4, 7, 1, 8, 0, -8]
+# arr = [-1,2,-3,4,5,8,9,-10,11,-12]
+Rearrange(arr)
