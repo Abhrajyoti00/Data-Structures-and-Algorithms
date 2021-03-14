9968f4701dcc716106591168f8071ea83fc706a6
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-03-14 22:51:32 +0530

    Count and Say problem added

---
+++ 450 Questions for DSA/Strings/53_Count and Say.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countAndSay(self, n):
+        count_and_say = [0 for i in range(n+2)]
+        count_and_say[1] = 1
+        count_and_say[2] = 11
+        for i in range(3,n+1):
+            prev = count_and_say[i-1]
+            str_prev = str(prev)
+            temp_list = []
+            count = 1
+            temp_string = ''
+            for j in range(0,len(str_prev)-1):
+                if str_prev[j] == str_prev[j+1]:
+                    count += 1
+                else:
+                    temp_string+= str(count)+str(str_prev[j])
+                    count = 1
+            temp_string+= str(count)+str(str_prev[j+1])
+            count_and_say[i] = int(temp_string)
+        return str(count_and_say[n])
+
+sol = Solution()
+print(sol.countAndSay(14))
+
+
