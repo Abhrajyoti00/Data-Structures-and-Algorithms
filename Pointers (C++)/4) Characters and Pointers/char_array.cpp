78b4bc64ef91b8b495cbd0383d7aa03e45e29e7c
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-04-02 12:50:27 +0530

    Characters and Pointers added

---
+++ Pointers (C++)/4) Characters and Pointers/char_array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    char str[] = "abcde";
+    char *pstr = "abcde"; // This is not correct as it is pointing to an address that might not work sometimes.
+
+    cout<<str<<endl;
+    cout<<pstr<<endl;
+
+
+
+    int a[] = {1,2,3};
+    char b[] = "abc";
+
+    cout<<a<<endl;
+    cout<<b<<endl; //char array is different
+
+    char *c = &b[0];
+    cout<<c<<endl; //Prints everything
+
+    char c1= 'a';
+    char *pc = &c1;
+
+    cout<<c1<<endl;
+    cout<<pc<<endl; // It continues to print until it finds a null character
+
+}
