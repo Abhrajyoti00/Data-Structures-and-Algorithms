94788d580ba5631d996b45cba808730045ce7c56
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-04-02 10:47:23 +0530

    Pointers 3 added

---
+++ Pointers (C++)/1) Introduction to Pointers/Pointers3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    cout<<i<<endl;
+    i++;
+    cout<<i<<endl;
+
+    int *p = 0; // Always initialize pointer to a null value.
+    cout<<p<<endl;
+    cout<<*p<<endl;
+    (*p)++;
+    cout<<*p<<endl;
+}
