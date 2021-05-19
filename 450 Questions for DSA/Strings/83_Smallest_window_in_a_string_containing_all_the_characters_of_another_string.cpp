6939943b7e1de02da65c78121c67cbea3aab57bc
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-05-19 22:06:37 +0530

    Two pointer method of Smallest Window substring added

--- 450 Questions for DSA/Strings/83_Smallest_window_in_a_string_containing_all_the_characters_of_another_string.cpp
+++ 450 Questions for DSA/Strings/83_Smallest_window_in_a_string_containing_all_the_characters_of_another_string.cpp
@@ -6,43 +6,49 @@ class Solution
     public:
     //Function to find the smallest window in the string s consisting
     //of all the characters of string p.
+
+    //////////////              TWO POINTER METHOD              ////////////
     string smallestWindow (string s, string p)
     {
+        
         map<char,int>pattern_map;
         map<char,int>string_map;
-        int map_count; //To count the total matched counts
+        int match_count = 0 ; //To count the total matched counts
         string ans = "";
         int min_len = INT_MAX;
         for(int i = 0; i<p.length();i++){
             pattern_map[p[i]]++;
         }
-        int popper = -1, inserter = 0;
-        while(inserter<s.length()){
+
+        int popper = -1, inserter = -1;
+        while(inserter<int(s.length())){
+                //    cout<<"here\n";
+
+            inserter++;
             string_map[s[inserter]]++;
-            if(string_map[s[inserter]]<=pattern_map[s[inserter]]){
-                map_count++;
+            if((pattern_map.find(s[inserter])!= pattern_map.end()) && string_map[s[inserter]]<=pattern_map[s[inserter]]){
+                match_count++;
             }
-            if(map_count == p.length()){
+            while(match_count == p.length()){
                 int cur_len = inserter-popper;
-                
-                while(popper<inserter){
-                    if(min_len>cur_len){
-                        ans = s.substr(popper+1, cur_len);
-                        cout<<"Substring ans"<<endl;
-                        min_len = cur_len;
-                    }
-                    if(string_map[s[popper]]<pattern_map[s[popper]]){
-                        map_count--;
-                        break;
-                    }
-                    string_map[s[popper]]--;
-                    popper++;
+            
+                if(min_len>cur_len){
+                    ans = s.substr(popper+1, cur_len);
+                    // cout<<"Substring ans  "<<ans<<endl;
+                    min_len = cur_len;
+                }
+                popper++;
+                string_map[s[popper]]--;
+
+                if((pattern_map.find(s[popper])!= pattern_map.end()) && string_map[s[popper]]<pattern_map[s[popper]]){
+                        match_count--;
                 }
             }
-            inserter++;
         }
-
-        return ans;
+        if(ans.length()>0)
+            return ans;
+        else
+            return "-1";
     }
 };
 
@@ -52,3 +58,6 @@ int main(){
     Solution sol;
     cout<<sol.smallestWindow(s,p);
 }
+//s = "dbaecbbabdcaafbddcabgba"
+//p = "abbcdc"
+//Output = "cbbabdc"
