350baa869715590ef15b559af8e552f58c86f1e1
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-06-03 00:58:20 +0530

    First and last occurence optimal sol added

--- 450 Questions for DSA/Searching & Sorting/90_First_and_last_occurrences_of_x.cpp
+++ 450 Questions for DSA/Searching & Sorting/90_First_and_last_occurrences_of_x.cpp
@@ -1,59 +1,109 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// O(n) Solution 
+// vector<int> find(int arr[], int n , int x )
+// {
+//     vector<int> ans;
+//     int first = 0, last = n-1, mid = 0;
+//     int flag = 0;
+//     while(first<=last){
+//         mid = (first+last)/2;
+//         if(x<arr[mid]){
+//             last = mid-1;
+//         }
+//         else if(x>arr[mid]){
+//             first = mid+1;
+//         }
+//         else if(x == arr[mid]){
+//             flag = 1;
+//             break;
+//         }
+//     }
+
+//     if(flag!=1){
+//         ans.push_back(-1);
+//         ans.push_back(-1);
+//         return ans;
+//     }
+//     // Now the mid has the ans
+
+//     int first_occurence = mid, last_occurence = mid;
+//     int mid1 = 0, mid2 = 0;
+//     // cout<<"Mid = "<<mid<<endl;
+//     while(first_occurence>=0){
+//         if(arr[first_occurence-1] == x){
+//             first_occurence--;
+//         }
+//         else{
+//             ans.push_back(first_occurence);
+//             break;
+//         }
+
+//     }
+
+//     while(last_occurence<n){
+//         if(arr[last_occurence+1] == x){
+//             last_occurence++;
+//         }        
+//         else{
+//             ans.push_back(last_occurence);
+//             break;
+//         }
+//     }
+
+
+//     return ans;
+// }
+
+ 
+// O(log N) solution :-
+
+
 vector<int> find(int arr[], int n , int x )
 {
     vector<int> ans;
-    int first = 0, last = n-1, mid = 0;
+    int low = 0, high = n-1, mid = 0, res = -1;
     int flag = 0;
-    while(first<=last){
-        mid = (first+last)/2;
+
+    // For finding the first occurence
+    while(low<=high){
+        mid = (low+high)/2;
         if(x<arr[mid]){
-            last = mid-1;
+            high = mid-1;
         }
         else if(x>arr[mid]){
-            first = mid+1;
+            low = mid+1;
         }
         else if(x == arr[mid]){
-            flag = 1;
-            break;
+            res = mid;
+            high = mid-1;
         }
     }
+    ans.push_back(res);
 
-    if(flag!=1){
-        ans.push_back(-1);
-        ans.push_back(-1);
-        return ans;
-    }
-    // Now the mid has the ans
-
-    int first_occurence = mid, last_occurence = mid;
-    int mid1 = 0, mid2 = 0;
-    // cout<<"Mid = "<<mid<<endl;
-    while(first_occurence>=0){
-        if(arr[first_occurence-1] == x){
-            first_occurence--;
+    low = 0, high = n-1, mid = 0, res = -1;
+    // For finding the last occurence
+    
+    while(low<=high){
+        mid = (low+high)/2;
+        if(x<arr[mid]){
+            high = mid-1;
         }
-        else{
-            ans.push_back(first_occurence);
-            break;
+        else if(x>arr[mid]){
+            low = mid+1;
         }
-
-    }
-
-    while(last_occurence<n){
-        if(arr[last_occurence+1] == x){
-            last_occurence++;
-        }        
-        else{
-            ans.push_back(last_occurence);
-            break;
+        else if(x == arr[mid]){
+            res = mid;
+            low = mid+1;
         }
     }
-
-
+    ans.push_back(res);
     return ans;
+
 }
+
+
 
 int main(){
     int n, x;
