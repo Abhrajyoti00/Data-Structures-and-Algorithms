3247db16ce819472c4d6c5467ec68eb917bae7c8
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-06-02 22:36:48 +0530

    First and last occurence of a number added

---
+++ 450 Questions for DSA/Searching & Sorting/90_First_and_last_occurrences_of_x.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> find(int arr[], int n , int x )
+{
+    vector<int> ans;
+    int first = 0, last = n-1, mid = 0;
+    int flag = 0;
+    while(first<=last){
+        mid = (first+last)/2;
+        if(x<arr[mid]){
+            last = mid-1;
+        }
+        else if(x>arr[mid]){
+            first = mid+1;
+        }
+        else if(x == arr[mid]){
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag!=1){
+        ans.push_back(-1);
+        ans.push_back(-1);
+        return ans;
+    }
+    // Now the mid has the ans
+
+    int first_occurence = mid, last_occurence = mid;
+    int mid1 = 0, mid2 = 0;
+    // cout<<"Mid = "<<mid<<endl;
+    while(first_occurence>=0){
+        if(arr[first_occurence-1] == x){
+            first_occurence--;
+        }
+        else{
+            ans.push_back(first_occurence);
+            break;
+        }
+
+    }
+
+    while(last_occurence<n){
+        if(arr[last_occurence+1] == x){
+            last_occurence++;
+        }        
+        else{
+            ans.push_back(last_occurence);
+            break;
+        }
+    }
+
+
+    return ans;
+}
+
+int main(){
+    int n, x;
+    cin>>n>>x;
+    int arr[n];
+    for(int i = 0 ; i<n; i++)
+        cin>>arr[i];
+    vector<int> ans = find(arr, n, x);
+    cout<<ans[0]<<" "<<ans[ans.size()-1]<<endl;
+}
+
+//n=9, x=5
+// arr[] = { 1, 3, 5, 5, 5, 5, 67, 123, 125 }
+// 1 3 5 5 5 5 67 123 125
