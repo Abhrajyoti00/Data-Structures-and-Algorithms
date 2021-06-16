ff38c8dccd472af06b43d52d10055dbaa5829543
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-06-16 21:06:45 +0530

    Added the most optimised sol for sort 012

--- 450 Questions for DSA/Array/04_Sort_012_using_Dutch_Algo.cpp
+++ 450 Questions for DSA/Array/04_Sort_012_using_Dutch_Algo.cpp
@@ -1,49 +1,69 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution{
-    public:
+class Solution
+{
+public:
+
+        // O(2n) using Dutch Algo 
+
+    // void sort012(int a[], int n)
+    // {
+    //     // To fix the position of 2
+    //     int i = 0, j = n-1;
+    //     while(i<=j){
+    //         if(a[j] == 2)
+    //             j--;
+    //         else if(a[i] != 2)
+    //             i++;
+    //         else if(a[i] == 2 && a[j]!=2){
+    //             swap(a[i], a[j]);
+    //             i++;
+    //             j--;
+    //         }
+    //     }
+    //     // cout<<j<<endl;
+    //     // To fix the pos of 0 and 1
+    //     // j contains the pos after which 2 starts
+    //     i = 0;
+    //     while(i<=j){
+    //         if(a[i] == 0)
+    //             i++;
+    //         else if(a[j] == 1)
+    //             j--;
+    //         else if(a[i] == 1 && a[j] == 0){
+    //             swap(a[i], a[j]);
+    //             i++;
+    //             j--;
+    //         }
+    //     }
+    // }
+
+    // O(n) using Dutch Algo , Best Method :- 
     void sort012(int a[], int n)
     {
-        // To fix the position of 2
-        int i = 0, j = n-1;
-        while(i<=j){
-            if(a[j] == 2)
-                j--;
-            else if(a[i] != 2)
-                i++;
-            else if(a[i] == 2 && a[j]!=2){
-                swap(a[i], a[j]);
-                i++;
-                j--;
-            }
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high)
+        {   
+            if(a[mid] == 0)
+                swap(a[mid++], a[low++]);
+            else if(a[mid] == 1)
+                mid++;
+            else if(a[mid] == 2)
+                swap(a[mid], a[high--]);
         }
-        // cout<<j<<endl;
-        // To fix the pos of 0 and 1
-        // j contains the pos after which 2 starts
-        i = 0;
-        while(i<=j){
-            if(a[i] == 0)
-                i++;
-            else if(a[j] == 1)
-                j--;
-            else if(a[i] == 1 && a[j] == 0){
-                swap(a[i], a[j]);
-                i++;
-                j--;
-            }
-        }
-    }    
+    }
 };
 
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i = 0 ; i <n ; i++)
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
     Solution sol;
     sol.sort012(arr, n);
-    for(int i = 0; i <n ; i++)
-        cout<<arr[i]<<" ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 }
