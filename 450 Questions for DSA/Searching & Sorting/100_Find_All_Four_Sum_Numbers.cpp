2949eef6bd467a1cd11e19b02ebf214b3ebf1060
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-07-27 10:29:46 +0530

    Solution done for q100

--- 450 Questions for DSA/Searching & Sorting/100_Find_All_Four_Sum_Numbers.cpp
+++ 450 Questions for DSA/Searching & Sorting/100_Find_All_Four_Sum_Numbers.cpp
@@ -1,36 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
+class Solution
+{
+public:
     // arr[] : int input array of integers
     // k : the quadruple sum required
-    vector<vector<int> > fourSum(vector<int> &arr, int s) {
+    vector<vector<int>> fourSum(vector<int> &arr, int s)
+    {
         int n;
-        vector<int>ans_small;
-        vector<vector<int>>ans_big;
-        
+        vector<int> ans_small;
+        vector<vector<int>> ans_big;
+
         n = arr.size();
         sort(arr.begin(), arr.end());
-        unordered_map<int, int> mp;
-        set<vector<int>>power_set;
-        for(int i = 0; i<= n-4; i++){
-            for(int j = i+1; j<=n-3;j++){
-                for(int k = j+1; k<=n-2; k++){
-                    int sum_reqd = s - (arr[i]+arr[j]+arr[k]);
-                    if(binary_search(arr.begin()+(k+1), arr.end(), sum_reqd))
-                        {
-                            ans_small.push_back(arr[i]);
-                            ans_small.push_back(arr[j]);
-                            ans_small.push_back(arr[k]);
-                            ans_small.push_back(sum_reqd);
-                        }
+        set<vector<int>> power_set;
+        for (int i = 0; i <= n - 4; i++)
+        {
+            for (int j = i + 1; j <= n - 3; j++)
+            {
+                for (int k = j + 1; k <= n - 2; k++)
+                {
+                    int sum_reqd = s - (arr[i] + arr[j] + arr[k]);
+                    if (binary_search(arr.begin() + (k + 1), arr.end(), sum_reqd))
+                    {
+                        ans_small.push_back(arr[i]);
+                        ans_small.push_back(arr[j]);
+                        ans_small.push_back(arr[k]);
+                        ans_small.push_back(sum_reqd);
                         power_set.insert(ans_small);
                         ans_small.clear();
-                }   
+                    }
+                }
             }
         }
-        for(auto iterator: power_set)
+        for (auto iterator : power_set)
             ans_big.push_back(iterator);
         return ans_big;
     }
@@ -50,21 +54,22 @@ int main()
     cin >> n;
     int arr[n];
     int k;
-    cin>>k;
-    vector<int>my_vec;
-    for (int i = 0; i < n; i++){
+    cin >> k;
+    vector<int> my_vec;
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
         my_vec.push_back(arr[i]);
     }
-    vector<vector<int>>ans;
+    vector<vector<int>> ans;
     Solution s;
     ans = s.fourSum(my_vec, k);
-    for(int i = 0; i<ans.size();i++)
+    for (int i = 0; i < ans.size(); i++)
     {
-        for(int j = 0; j<ans[i].size();j++){
-            cout<<ans[i][j]<<" ";
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
         }
-        cout<<"$";
+        cout << "$";
     }
-
 }
