0e4836a3ecd46cb3559064e1bbd4ec4d6460c102
Abhrajyoti00 <abhrajyoti00@gmail.com>
2023-06-13 13:12:55 +0530

    added sumofdivisors

---
+++ Striver_A2Z_DSA/sumOfDiv.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    
+    long long sumOfDivisors(int N)
+    {
+        long long sum = 0;
+        for(int i = 1; i<=N; i++){
+            int div = N/i;
+            sum+= div*i;
+        }
+        return sum;
+    }
+
+};
