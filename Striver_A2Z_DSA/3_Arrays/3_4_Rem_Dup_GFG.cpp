ac56c43b86c6d939c6cad08f849e17d3279e0288
Abhrajyoti00 <abhrajyoti00@gmail.com>
2023-06-22 09:51:49 +0530

    Added 3 more probs

---
+++ Striver_A2Z_DSA/3_Arrays/3_4_Rem_Dup_GFG.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int remove_duplicate(int arr[],int n){
+        int j = 0;
+        for(int i = 0 ; i<n-1; i++){
+            if(arr[i]!=arr[i+1]){
+                arr[j++] = arr[i];
+            }
+        }
+        arr[j++] = arr[n-1];
+        return j;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int a[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>a[i];
+        }
+    Solution ob;
+    int n = ob.remove_duplicate(a,N);
+
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    }
+}
+// } Driver Code Ends
